929ccadb251a570138df3f9bbeb6bb472ade52b6
DESKTOP-SADEM6I\HOME <phanhien9218@gmail.com>
2017-07-07 00:30:11 +0700

    pagination ajax

--- AjaxJqueryTrongASPMVC/Scripts/Controller/homeController.js
+++ AjaxJqueryTrongASPMVC/Scripts/Controller/homeController.js
@@ -1,4 +1,8 @@
-﻿var homeController = {
+﻿var homeconfig = {
+    pageSize: 3,
+    pageIndex: 1,
+}
+var homeController = {
     init: function () {
         homeController.loadData();
         homeController.registerEvent();
@@ -12,24 +16,24 @@
                 homeController.updateSalary(id, value);
             }
         });
-        
     },
     updateSalary: function (id, value) {
         var data = {
             ID: id,
-            Salary:value
+            Salary: value
         };
         $.ajax({
             url: '/Home/Update',
             type: 'POST',
             dataType: 'json',
-            data: {model: JSON.stringify(data)},
+            data: { model: JSON.stringify(data) },
             success: function (response) {
                 if (response.status) {
-                    alert('update successed')
+                    alert('Update successed.');
                 }
-                else
-                    alert('update faled');
+                else {
+                    alert('Update failed.');
+                }
             }
         })
     },
@@ -37,26 +41,48 @@
         $.ajax({
             url: '/Home/LoadData',
             type: 'GET',
+            data: {
+                page: homeconfig.pageIndex,
+                pageSize: homeconfig.pageSize
+            },
             dataType: 'json',
             success: function (response) {
                 if (response.status) {
                     var data = response.data;
                     var html = '';
                     var template = $('#data-template').html();
-                    $.each(data, function (i,item) {
+                    $.each(data, function (i, item) {
                         html += Mustache.render(template, {
                             ID: item.ID,
-                            Name:item.Name,
-                            Salary:item.Salary,
-                            Status:item.Status ==true?"<span class=\"label label-success\">Active</span>":"<span class=\"label label-danger\">Locked</span>"
+                            Name: item.Name,
+                            Salary: item.Salary,
+                            Status: item.Status == true ? "<span class=\"label label-success\">Actived</span>" : "<span class=\"label label-danger\">Locked</span>"
                         });
-                
+
                     });
                     $('#tblData').html(html);
+                    homeController.paging(response.total, function () {
+                        homeController.loadData();
+                    });
                     homeController.registerEvent();
                 }
             }
         })
+    },
+    paging: function (totalRow, callback) {
+        var totalPage = Math.ceil(totalRow / homeconfig.pageSize);
+        $('#pagination').twbsPagination({
+            total: totalPage,
+            first: "Đầu",
+            next: "Tiếp",
+            last: "Cuối",
+            prev: "Trước",
+            visiblePages: 10,
+            onPageClick: function (event, page) {
+                homeconfig.pageIndex = page;
+                setTimeout(callback, 200);
+            }
+        });
     }
 }
 homeController.init();
